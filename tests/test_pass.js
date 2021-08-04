82c856ddf2af4f77fbeaadf62977e2e66859b806
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2021-08-04 21:53:55 +0530

    fixed a pesky bug

--- tests/test_pass.js
+++ tests/test_pass.js
@@ -3,6 +3,6 @@ describe('ENTERING NAME', function() {
     browser.get('https://angularjs.org');
     element(by.model('yourName')).sendKeys('SAJEEV KRISHNA');
     var guru= element(by.xpath('html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
-    expect(guru.getText()).toEqual('Hello SAJEEV KRISHNA');
+    expect(guru.getText()).toEqual('Hello SAJEEV KRISHNA!');
     });
   });
