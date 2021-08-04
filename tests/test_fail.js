68f0cb21d46cecbd4f4af8c254a9ce637809b363
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2021-08-04 21:32:43 +0530

    failed test

---
+++ tests/test_fail.js
@@ -0,0 +1,8 @@
+describe('ENTERING NAME', function() {
+    it('SHOULD DISPLAY "HELLO NAME"', function() {
+    browser.get('https://angularjs.org');
+    element(by.model('yourName')).sendKeys('SAJEEV KRISHNA');
+    var guru= element(by.xpath('html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
+    expect(guru.getText()).toEqual('Hello SAJEEV KRISHNA!!!');
+    });
+  });
