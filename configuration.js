45ada57738c717d56c70e139893dd4931f661666
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2021-08-04 21:34:24 +0530

    Update

--- configuration.js
+++ configuration.js
@@ -1,6 +1,6 @@
 exports.config = {
       seleniumAddress: 'http://localhost:4444/wd/hub',
-      specs: ['test_pass.js','test_fail.js'],
+      specs: ['./tests/test_pass.js','./tests/test_fail.js'],
       framework: 'jasmine2' ,
       onPrepare: function() {
           var jasmineReporters='E:\\NODE\\node_modules\\jasmine-reporters'
